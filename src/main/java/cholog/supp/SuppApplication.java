662f3148cf9f58c9e32d35ee70337dbe0641ff44
Sumin <new.sumyang@gmail.com>
2024-09-26 17:31:42 +0900

    init commit

---
+++ src/main/java/cholog/supp/SuppApplication.java
@@ -0,0 +1,13 @@
+package cholog.supp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cholog/supp/SuppApplicationTests.java
@@ -0,0 +1,13 @@
+package cholog.supp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
