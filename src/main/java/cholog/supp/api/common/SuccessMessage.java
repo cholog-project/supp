04a698f76f527d156f9edcc190fa9ffa27bf3f1c
Sumin <new.sumyang@gmail.com>
2024-10-08 17:27:43 +0900

    feat: SuccessMessage enum 작성

---
+++ src/main/java/cholog/supp/api/common/SuccessMessage.java
@@ -0,0 +1,23 @@
+package cholog.supp.api.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum SuccessMessage {
+
+    GET_POSTS_SUCCESS("질문 목록 조회 성공", HttpStatus.OK.value()),
+    GET_POST_SUCCESS("단일 질문 조회 성공", HttpStatus.OK.value()),
+    WRITE_POST_SUCCESS("질문글 작성 성공", HttpStatus.CREATED.value()),
+    DELETE_POST_SUCCESS("질문글 삭제 성공", HttpStatus.NO_CONTENT.value()),
+    MODIFY_POST_SUCCESS("질문글 수정 성공", HttpStatus.OK.value()),
+    WRITE_COMMENT_SUCCESS("의견 작성 성공", HttpStatus.OK.value()),
+    DELETE_COMMENT_SUCCESS("의견 삭제 성공", HttpStatus.NO_CONTENT.value()),
+    MODIFY_COMMENT_SUCCESS("의견 수정 성공", HttpStatus.OK.value())
+    ;
+
+    private final String message;
+    private final int status;
+}
