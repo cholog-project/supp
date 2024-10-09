8fdad8cadd588b83dd8c85368ba91778809957e9
Sumin <new.sumyang@gmail.com>
2024-10-09 16:47:09 +0900

    feat: 특정 사용자의 스터디 그룹조회 기능 개발
    Co-authored-by: choi-jjunho <junho5336@gmail.com>

--- src/main/java/cholog/supp/api/study/controller/StudyGroupController.java
+++ src/main/java/cholog/supp/api/study/controller/StudyGroupController.java
@@ -2,12 +2,16 @@ package cholog.supp.api.study.controller;
 
 import cholog.supp.api.common.APISuccessResponse;
 import cholog.supp.api.study.dto.CreateStudyGroupRequest;
+import cholog.supp.api.study.dto.StudyGroupResponse;
 import cholog.supp.api.study.service.StudyGroupService;
 import cholog.supp.common.auth.Auth;
 import cholog.supp.db.member.Member;
 import cholog.supp.db.member.MemberCategoryRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +29,13 @@ public class StudyGroupController {
         @RequestBody CreateStudyGroupRequest request, @Auth Member member) {
         studyGroupService.createStudyGroup(request, member);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/group")
+    public ResponseEntity<List<StudyGroupResponse>> getGroup(
+        @Auth Member member
+    ) {
+        var response = studyGroupService.getGroup(member);
+        return ResponseEntity.ok().body(response);
     }
 }
