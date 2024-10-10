af5e6cccb8673ed723778cd66bdd5fc713d8a9c4
Sumin <new.sumyang@gmail.com>
2024-10-10 18:17:53 +0900

    feat: 엔티티, 레파지토리생성

---
+++ src/main/java/cholog/supp/db/comment/Comment.java
@@ -0,0 +1,43 @@
+package cholog.supp.db.comment;
+
+import cholog.supp.db.member.Member;
+import cholog.supp.db.post.Post;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "COMMENT")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @NotNull
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
+
+}
---
+++ src/main/java/cholog/supp/db/comment/CommentRepository.java
@@ -0,0 +1,7 @@
+package cholog.supp.db.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
