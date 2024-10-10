e8252d1d5cf2e7dd3ab51de6aaaf88f68f4150fd
Sumin <new.sumyang@gmail.com>
2024-10-10 17:50:00 +0900

    style: 줄변경

--- src/main/java/cholog/supp/api/post/controller/PostController.java
+++ src/main/java/cholog/supp/api/post/controller/PostController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/post")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/post")
 public class PostController {
 
     private final PostService postService;
