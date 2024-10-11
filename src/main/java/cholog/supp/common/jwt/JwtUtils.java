310a6500d74cb477e93daca83d6034f22f9d0509
Sumin <new.sumyang@gmail.com>
2024-10-11 17:41:11 +0900

    feat: 만료시간 추가

--- src/main/java/cholog/supp/common/jwt/JwtUtils.java
+++ src/main/java/cholog/supp/common/jwt/JwtUtils.java
@@ -3,6 +3,7 @@ package cholog.supp.common.jwt;
 import cholog.supp.db.member.MemberType;
 import cholog.supp.db.study.StudyGroup;
 import io.jsonwebtoken.Jwts;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +13,14 @@ public class JwtUtils {
     @Value("${jwt.secret}")
     private String secretKey;
 
+    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
+
     public String createToken(StudyGroup studyGroup, MemberType memberType) {
         return Jwts.builder()
             .subject(studyGroup.getId().toString())
             .claim("organization", studyGroup.getOrganization())
             .claim("memberType", memberType)
+            .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
             .compact();
     }
 }
