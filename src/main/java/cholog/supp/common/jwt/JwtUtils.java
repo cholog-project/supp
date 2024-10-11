887fa4bad83c0d4ddf404fb25e417aec6d5ea6ca
Sumin <new.sumyang@gmail.com>
2024-10-11 18:04:05 +0900

    refactor: 토큰 생성 로직 수정

--- src/main/java/cholog/supp/common/jwt/JwtUtils.java
+++ src/main/java/cholog/supp/common/jwt/JwtUtils.java
@@ -3,6 +3,7 @@ package cholog.supp.common.jwt;
 import cholog.supp.db.member.MemberType;
 import cholog.supp.db.study.StudyGroup;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -13,14 +14,16 @@ public class JwtUtils {
     @Value("${jwt.secret}")
     private String secretKey;
 
-    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
+    @Value("${jwt.token.access-expiration-time}")
+    private long expirationTime;
 
     public String createToken(StudyGroup studyGroup, MemberType memberType) {
         return Jwts.builder()
             .subject(studyGroup.getId().toString())
             .claim("organization", studyGroup.getOrganization())
             .claim("memberType", memberType)
-            .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+            .expiration(new Date(System.currentTimeMillis() + expirationTime))
             .compact();
     }
 }
