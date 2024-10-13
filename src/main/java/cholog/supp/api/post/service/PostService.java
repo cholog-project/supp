f7e9a31978b9463cad246f15fc421ac006ec3cd5
Sumin <new.sumyang@gmail.com>
2024-10-13 14:11:09 +0900

    단일 스터디 api 복원

--- src/main/java/cholog/supp/api/post/service/PostService.java
+++ src/main/java/cholog/supp/api/post/service/PostService.java
@@ -9,7 +9,6 @@ import cholog.supp.db.member.MemberStudyMapRepository;
 import cholog.supp.db.post.Post;
 import cholog.supp.db.post.PostRepository;
 import cholog.supp.db.study.StudyGroup;
-import cholog.supp.db.study.StudyGroupRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,7 +21,6 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final MemberStudyMapRepository memberStudyMapRepository;
-    private final StudyGroupRepository studyGroupRepository;
 
     public void createPost(Member member, CreatePostRequest createPostRequest) {
         MemberStudyMap memberStudy = memberStudyMapRepository.findByStudyGroupIdAndMemberId(
