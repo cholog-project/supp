2d5bd6669f4bd07f8a46a9575d22fd2f15823836
Sumin <new.sumyang@gmail.com>
2024-10-13 13:53:39 +0900

    merge

--- src/main/java/cholog/supp/api/member/controller/MemberController.java
+++ src/main/java/cholog/supp/api/member/controller/MemberController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
