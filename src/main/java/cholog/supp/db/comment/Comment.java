0d4d1240c0e0b19c57f72d70fbf391ccacd46bc3
Sumin <new.sumyang@gmail.com>
2024-10-14 16:14:03 +0900

    refactor: abstract class 추가

--- src/main/java/cholog/supp/db/comment/Comment.java
+++ src/main/java/cholog/supp/db/comment/Comment.java
@@ -1,10 +1,10 @@
 package cholog.supp.db.comment;
 
+import cholog.supp.db.AuditEntity;
 import cholog.supp.db.member.Member;
 import cholog.supp.db.post.Post;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,19 +14,14 @@ import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Entity
 @Table(name = "COMMENT")
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class Comment {
+public class Comment extends AuditEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -47,15 +42,6 @@ public class Comment {
     @Lob
     @Column(name = "content", nullable = false)
     private String content;
-
-    @CreatedDate
-    @Column(name = "created_date", nullable = false)
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    @Column(name = "modified_date")
-    private LocalDateTime modifiedDate;
-
 
     public Comment(Member member, Post post, String content) {
         this.member = member;
--- src/main/java/cholog/supp/db/post/Post.java
+++ src/main/java/cholog/supp/db/post/Post.java
@@ -1,11 +1,11 @@
 package cholog.supp.db.post;
 
 import cholog.supp.api.post.dto.ModifyPost;
+import cholog.supp.db.AuditEntity;
 import cholog.supp.db.member.Member;
 import cholog.supp.db.study.StudyGroup;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,19 +16,14 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Entity
 @Table(name = "POST")
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class Post {
+public class Post extends AuditEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -54,14 +49,6 @@ public class Post {
     @Lob
     @Column(name = "description", nullable = false)
     private String description;
-
-    @CreatedDate
-    @Column(name = "created_date", nullable = false)
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    @Column(name = "modified_date")
-    private LocalDateTime modifiedDate;
 
     public Post(Member member, StudyGroup study, String title, String description) {
         this.member = member;
