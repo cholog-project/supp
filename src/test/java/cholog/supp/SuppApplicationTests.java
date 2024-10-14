befc37023b6778c7d0c71256f6c60a8a64ab7c65
boyekim <122220985+boyekim@users.noreply.github.com>
2024-10-14 21:28:02 +0900

    modify: 테스트 annotation (#39)

--- src/test/java/cholog/supp/SuppApplicationTests.java
+++ src/test/java/cholog/supp/SuppApplicationTests.java
@@ -3,7 +3,7 @@ package cholog.supp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SuppApplicationTests.class)
 class SuppApplicationTests {
 
     @Test
